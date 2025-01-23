790fe20f4baf423402fe3861e8321a90c58a08d6
rjohn05-dc <rohanmathewjohn@gmail.com>
2025-01-22 20:16:30 -0700

    added a banner

---
+++ script.js
@@ -0,0 +1,9 @@
+const gameInfo={
+    header:"Black Myth Wukong",
+    bannerImage:{
+        image:"https://preview.redd.it/final-thoughts-before-black-myth-wukongs-launch-v0-u8pww3tlkjjd1.jpeg?width=640&crop=smart&auto=webp&s=d6ea2ad812532b0a7175881cf2a428dc7fbefdbf",
+        width:"500px",
+        height:"250px",
+        alt:"Black Myth Wukong Banner"
+}
+}
