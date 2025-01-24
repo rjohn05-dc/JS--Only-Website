19d9cbc7d80082ca69bfbdf8c2be8b40d95d5c5c
rjohn05-dc <rohanmathewjohn@gmail.com>
2025-01-23 18:19:39 -0700

    fjdfji

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 const gameInfo={
     header:"Black Myth Wukong",
     bannerImage:{
-        image:"https://preview.redd.it/final-thoughts-before-black-myth-wukongs-launch-v0-u8pww3tlkjjd1.jpeg?width=640&crop=smart&auto=webp&s=d6ea2ad812532b0a7175881cf2a428dc7fbefdbf",
-        width:"500px",
-        height:"250px",
-        alt:"Black Myth Wukong Banner"
+        image:"https://cdn11.bigcommerce.com/s-uv3gptffbq/images/stencil/original/image-manager/black-myth-wukong-banner.png?t=1727769031",
+        width:"100%",
+        height:"400px",
+        alt:"Black Myth Wukong Banner",
+},
+navigation:["Home","About","Gallery"],
+
 }
+const mythUpdate=(tag,parent,attributes={},content="")=>{
+    const element=document.createElement(tag);
+    Object.keys(attributes).forEach(key=>element.setAttribute(key,attributes[key]));
+    element.innerHTML=content;
+    parent.appendChild(element);
+    return element;
 }
+
+// Creating the Header Section
+const header=mythUpdate("h1",document.body,{class:"game-header"}, gameInfo.header)
+
+// Create the Banner Section
+const banner=mythUpdate("banner",document.body,{class:"game-banner"})
+mythUpdate('img',banner,{src:gameInfo.bannerImage.image, alt:gameInfo.bannerImage.alt, width:gameInfo.bannerImage.width,height:gameInfo.bannerImage.height})
+
+
+
+// Creating the Nav Bar
+
+const nav=mythUpdate("nav",document.body,{class:"game-nav"})
+const ul =mythUpdate("ul",nav,{class:"list"})
+
+gameInfo.navigation.forEach(link=>{
+    mythUpdate("li",ul,{class:"text"},`<a href="#">${link}</a>`)
+})
+
+// Creating the Main Section
+
+// Creating the Gallery Section
+
+// const monkeyArmor=`
+// header{text-align:center;}
+
+// `
+// mythUpdate("monkeyArmor",document.head,{},monkeyArmor)
+
